3a0aeda3adc4a247bf9f690f5ee22bf7ed37d6bf
lord-charles <mwanikicharles226@gmail.com>
2023-09-20 16:04:12 +0300

    43

--- app.js
+++ app.js
@@ -54,6 +54,17 @@ app.use("/google", googleRouter);
 app.use(notFound);
 app.use(errorHandler);
 
+// Enable CORS for a specific domain
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ellahomes.charlesmwaniki.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
